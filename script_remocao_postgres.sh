1ff2b23bf74f6a17e519cbdbf432f1621673fd9e
DevApps <89113684+devappsteam@users.noreply.github.com>
2023-12-11 07:57:05 -0300

    Create script_remocao_postgres.sh

---
+++ script_remocao_postgres.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Este script remove completamente o PostgreSQL de sistemas baseados em Debian.
+
+# Verifica se o sistema usa o gerenciador de pacotes apt
+if command -v apt-get &>/dev/null; then
+    	echo "Verificando a presença do PostgreSQL..."
+
+	echo -e "\n\n"
+	echo "************************************"
+	echo "   Parando o serviço do postgress   "
+	echo "************************************"
+
+	# 1. Para o PostgreSQL, se estiver em execução
+	sudo service postgresql stop
+
+	echo -e "\n\n"
+	echo "************************************"
+	echo "       Removendo os Pacotes	  "
+	echo "************************************"
+	# 2. Remove os pacotes do PostgreSQL
+	sudo apt-get -y purge postgresql postgresql-contrib
+
+	echo -e "\n\n"
+	echo "************************************"
+	echo "       Removendo os Usuários	  "
+	echo "************************************"
+	# 3. Remove o usuário e o grupo do PostgreSQL
+	sudo deluser postgres
+	sudo delgroup postgres
+
+	echo -e "\n\n"
+	echo "************************************"
+	echo "       Removendo as Configurções	  "
+	echo "************************************"
+	# 4. Remove os arquivos de configuração do PostgreSQL
+	sudo rm -r /etc/postgresql/
+
+	# 5. Remove o diretório de dados do PostgreSQL
+	sudo rm -r /var/lib/postgresql/
+
+	echo -e "\n\n"
+	echo "************************************"
+	echo "   Atualizando o cache de pacotes	  "
+	echo "************************************"
+	# 6. Atualiza o cache de pacotes
+	sudo apt-get update
+	echo -e "\n\n"
+	echo "O PostgreSQL foi removido completamente do sistema."
+else
+	echo -e "\n\n"
+	echo "Este script suporta apenas sistemas baseados em Debian. Saindo..."
+	exit 1
+fi
